5d5d860f36c792adab51570ce000114c8754b328
Manel Ariapala <amanelaa@yahoo.com>
2023-09-11 14:17:40 +0200

    first commit

--- wise_blog/src/app/contact/loading.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import styles from "./page.module.css";
-
-const Loading = () => {
-  return <div className={styles.container}>Loading</div>;
-};
-
-export default Loading;
